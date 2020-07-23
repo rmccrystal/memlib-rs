56ddcf1314a9d7e798ce253e7845b4e40d6ba6fc
Ryan McCrystal <rrmccrystal@gmail.com>
2020-07-23 12:34:06 -1000

    Added more features to memory scanner

--- src/memory/scan.rs
+++ src/memory/scan.rs
@@ -1,18 +1,27 @@
 use super::{Address, Handle};
 
 use log::*;
-use std::cmp::PartialEq;
+use std::cmp::{Eq, Ord};
 use std::marker::PhantomData;
+use crate::memory::read_memory;
 
 pub struct MemoryScan<T> {
-    pub matches: Vec<Address>,
+    pub matches: Vec<(Address, T)>,
     _marker: PhantomData<T>,
+}
+
+pub enum ScanValue<T> {
+    Value(T),
+    Increased,
+    Decreased,
+    Static,
+    Changed,
 }
 
 /// Implements cheat engine like memory scanning
 impl<T> MemoryScan<T>
-where
-    T: PartialEq + Sized,
+    where
+        T: Eq + Ord + Sized + Clone,
 {
     /// Cheat engine like memory scanning
     /// Creates a MemoryScan object containing addresses within address_range
@@ -42,16 +51,29 @@ where
     /// Creates a MemoryScan from a list of addresses
     pub fn from_addresses(addresses: Vec<Address>) -> MemoryScan<T> {
         debug!("Created a memory scan with {} addresses", addresses.len());
+
+        let matches = addresses.iter().map(|&addr| (addr, read_memory::<T>(addr))).collect();
+
         MemoryScan {
-            matches: addresses,
+            matches: matches,
             _marker: PhantomData,
         }
     }
 
     /// Filters the matches with values which match the new scan
-    pub fn scan(&mut self, handle: &Handle, value: T) {
+    pub fn scan(&mut self, handle: &Handle, scan_value: ScanValue<T>) {
         debug!("Scanning {} addresses", self.matches.len());
-        self.matches
-            .retain(|&address| handle.read_memory::<T>(address) == value);
+        self.matches.retain(|match_item| {
+            let address = match_item.0;
+            let old_value = match_item.1.clone();
+            let new_value = handle.read_memory::<T>(address);
+            match &scan_value {
+                ScanValue::Value(value) => new_value == *value,
+                ScanValue::Increased => new_value > old_value,
+                ScanValue::Decreased => new_value < old_value,
+                ScanValue::Static => new_value == old_value,
+                ScanValue::Changed => new_value != old_value,
+            }
+        });
     }
 }
