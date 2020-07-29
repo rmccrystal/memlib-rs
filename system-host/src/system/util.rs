007d8916e1759e9061931a02990bd578ba6f5f1c
Ryan McCrystal <rrmccrystal@gmail.com>
2020-07-28 18:53:25 -1000

    Logging

--- system-host/src/system/util.rs
+++ system-host/src/system/util.rs
@@ -1,15 +1,18 @@
 #![cfg(windows)]
 
 use winapi::um::*;
+use log::*;
 use std::mem;
 
 // Gets a down or up state of a certain key using a VK key code:
 // https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
 pub fn get_key_state(key: i32) -> bool {
+    debug!("get_key_state({})", key);
     unsafe { winuser::GetAsyncKeyState(key) != 0 }
 }
 
 pub fn move_mouse_relative(dx: i32, dy: i32) {
+    debug!("move_mouse_relative({}, {})", dx, dy);
     let mut input_union: winuser::INPUT_u = unsafe { std::mem::zeroed() };
     unsafe {
         input_union.mi_mut().dx = dx;
