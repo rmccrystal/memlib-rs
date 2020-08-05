4ffa6e2281d59ba2de4b65b323f8485ce6a7368f
Ryan McCrystal <rrmccrystal@gmail.com>
2020-08-04 22:52:23 -1000

    Updated

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let mut o = 0;
     loop {
         overlay.begin();
-        overlay.draw_text((10 + o, 10 + o * 2), "NIGGER!!!!!".parse().unwrap(), Color::from_rgb(255, 0, 0), 100);
+        overlay.draw_text((10 + o, 10 + o * 2), "test".parse().unwrap(), Color::from_rgb(255, 0, 0), 100);
         overlay.end();
         std::thread::sleep(std::time::Duration::from_millis(100));
         o += 1;
