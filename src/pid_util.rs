5f8184b81975bafd5e14c6d8c7bcc1e63665c506
Ryan McCrystal <rrmccrystal@gmail.com>
2022-06-05 16:00:27 -0700

    Renamed Pid traits to Context traits

--- src/pid_util.rs
+++ src/pid_util.rs
@@ -35,157 +35,157 @@ pub trait ProcessAttachInto: Sized {
 }
 
 impl<T, P> ProcessAttach for T
-    where T: Clone + GetPid<Pid=P>,
+    where T: Clone + GetContext<Context=P>,
 {
     type ProcessType = AttachedProcess<Self, P>;
 
     fn attach(&self, process_name: &str) -> Option<Self::ProcessType> {
-        self.get_pid_from_name(process_name)
-            .map(|pid| AttachedProcess::new(self.clone(), pid))
+        self.get_context_from_name(process_name)
+            .map(|ctx| AttachedProcess::new(self.clone(), ctx))
     }
 
     fn attach_pid(&self, pid: u32) -> Option<Self::ProcessType> {
-        self.get_pid_from_windows_pid(pid)
-            .map(|win_pid| AttachedProcess::new(self.clone(), win_pid))
+        self.get_context_from_pid(pid)
+            .map(|ctx| AttachedProcess::new(self.clone(), ctx))
     }
 
     fn attach_current(&self) -> Self::ProcessType {
-        let pid = self.get_current_pid();
-        AttachedProcess::new(self.clone(), pid)
+        let ctx = self.get_current_context();
+        AttachedProcess::new(self.clone(), ctx)
     }
 }
 
 impl<T, P> ProcessAttachInto for T
     where
-        T: GetPid<Pid=P>,
+        T: GetContext<Context=P>,
 {
     type ProcessType = AttachedProcess<Self, P>;
 
     fn attach_into(self, process_name: &str) -> Option<Self::ProcessType> {
-        self.get_pid_from_name(process_name)
-            .map(|pid| AttachedProcess::new(self, pid))
+        self.get_context_from_name(process_name)
+            .map(|ctx| AttachedProcess::new(self, ctx))
     }
 
     fn attach_into_pid(self, pid: u32) -> Option<Self::ProcessType> {
-        self.get_pid_from_windows_pid(pid)
-            .map(|win_pid| AttachedProcess::new(self, win_pid))
+        self.get_context_from_pid(pid)
+            .map(|ctx| AttachedProcess::new(self, ctx))
     }
 
     fn attach_into_current(self) -> Self::ProcessType {
-        let pid = self.get_current_pid();
-        AttachedProcess::new(self, pid)
+        let ctx = self.get_current_context();
+        AttachedProcess::new(self, ctx)
     }
 }
 
 /// Gets the Pid type from a process name, a windows PID, or the current PID
-pub trait GetPid {
-    type Pid;
+pub trait GetContext {
+    type Context;
 
-    fn get_pid_from_name(&self, process_name: &str) -> Option<Self::Pid>;
-    fn get_pid_from_windows_pid(&self, pid: u32) -> Option<Self::Pid>;
-    fn get_current_pid(&self) -> Self::Pid;
+    fn get_context_from_name(&self, process_name: &str) -> Option<Self::Context>;
+    fn get_context_from_pid(&self, pid: u32) -> Option<Self::Context>;
+    fn get_current_context(&self) -> Self::Context;
 }
 
 pub struct AttachedProcess<T, P> {
-    api: T,
-    pid: P,
+    pub api: T,
+    pub context: P,
 }
 
 impl<T, P> AttachedProcess<T, P> {
-    pub fn new(api: T, pid: P) -> Self {
-        Self { api, pid }
+    pub fn new(api: T, context: P) -> Self {
+        Self { api, context }
     }
 }
 
 /// A trait that mirrors the MemoryRead trait but reads from a PID instead of directly from the implementor.
 /// Note that the Pid type is not necessarily a Windows process ID. One may implement this using another form of identifier such as a dirbase.
-pub trait MemoryReadPid: GetPid {
+pub trait MemoryReadPid: GetContext {
     /// Reads memory from the process with the given PID.
-    fn try_read_bytes_into_pid(&self, pid: &Self::Pid, address: u64, buffer: &mut [u8]) -> Option<()>;
+    fn try_read_bytes_into_pid(&self, ctx: &Self::Context, address: u64, buffer: &mut [u8]) -> Option<()>;
 }
 
 impl<T, P> MemoryRead for AttachedProcess<T, P>
     where
-        T: MemoryReadPid<Pid=P>
+        T: MemoryReadPid<Context=P>
 {
     fn try_read_bytes_into(&self, address: u64, buffer: &mut [u8]) -> Option<()> {
-        self.api.try_read_bytes_into_pid(&self.pid, address, buffer)
+        self.api.try_read_bytes_into_pid(&self.context, address, buffer)
     }
 }
 
 /// A trait that mirrors the MemoryWrite trait but writes to a PID instead of directly from the implementor.
 /// Note that the Pid type is not necessarily a Windows process ID. One may implement this using another form of identifier such as a dirbase.
-pub trait MemoryWritePid: GetPid {
+pub trait MemoryWritePid: GetContext {
     /// Writes memory to the process with the given PID.
-    fn try_write_bytes_pid(&self, pid: &Self::Pid, address: u64, buffer: &[u8]) -> Option<()>;
+    fn try_write_bytes_pid(&self, ctx: &Self::Context, address: u64, buffer: &[u8]) -> Option<()>;
 }
 
 impl<T, P> MemoryWrite for AttachedProcess<T, P>
     where
-        T: MemoryWritePid<Pid=P>,
+        T: MemoryWritePid<Context=P>,
 {
     fn try_write_bytes(&self, address: u64, buffer: &[u8]) -> Option<()> {
-        self.api.try_write_bytes_pid(&self.pid, address, buffer)
+        self.api.try_write_bytes_pid(&self.context, address, buffer)
     }
 }
 
 /// A trait that mirrors the ModuleList trait by gets information from a PID instead of directly from the implementor.
 /// Note that the Pid type is not necessarily a Windows process ID. One may implement this using another form of identifier such as a dirbase.
-pub trait ModuleListPid: GetPid {
+pub trait ModuleListPid: GetContext {
     /// Returns a list of all modules from a Pid. If the implementor can only
     /// provide a single module based on the name, this function should panic
-    fn get_module_list(&self, pid: &Self::Pid) -> Vec<Module>;
+    fn get_module_list(&self, pid: &Self::Context) -> Vec<Module>;
 
     /// Returns a single module by name from the Pid.
     /// If the module name does not exist, returns None
-    fn get_module(&self, pid: &Self::Pid, name: &str) -> Option<Module> {
+    fn get_module(&self, pid: &Self::Context, name: &str) -> Option<Module> {
         self.get_module_list(pid)
             .into_iter()
             .find(|m| m.name.to_lowercase() == name.to_lowercase())
     }
 
     /// Gets the main module from the Pid.
-    fn get_main_module(&self, pid: &Self::Pid) -> Module;
+    fn get_main_module(&self, pid: &Self::Context) -> Module;
 }
 
 impl<T, P> ModuleList for AttachedProcess<T, P>
     where
-        T: ModuleListPid<Pid=P>,
+        T: ModuleListPid<Context=P>,
 {
     fn get_module_list(&self) -> Vec<Module> {
-        self.api.get_module_list(&self.pid)
+        self.api.get_module_list(&self.context)
     }
 
     fn get_module(&self, name: &str) -> Option<Module> {
-        self.api.get_module(&self.pid, name)
+        self.api.get_module(&self.context, name)
     }
 
     fn get_main_module(&self) -> Module {
-        self.api.get_main_module(&self.pid)
+        self.api.get_main_module(&self.context)
     }
 }
 
 /// A trait that mirrors the ProcessInfo trait by gets information from a PID instead of directly from the implementor.
 /// Note that the Pid type is not necessarily a Windows process ID. One may implement this using another form of identifier such as a dirbase.
-pub trait ProcessInfoPid: GetPid {
-    fn process_name(&self, pid: &Self::Pid) -> String;
-    fn peb_base_address(&self, pid: &Self::Pid) -> u64;
-    fn pid(&self, pid: &Self::Pid) -> u32;
+pub trait ProcessInfoPid: GetContext {
+    fn process_name(&self, pid: &Self::Context) -> String;
+    fn peb_base_address(&self, pid: &Self::Context) -> u64;
+    fn pid(&self, pid: &Self::Context) -> u32;
 }
 
 impl<T, P> ProcessInfo for AttachedProcess<T, P>
     where
-        T: ProcessInfoPid<Pid=P>,
+        T: ProcessInfoPid<Context=P>,
 {
     fn process_name(&self) -> String {
-        self.api.process_name(&self.pid)
+        self.api.process_name(&self.context)
     }
 
     fn peb_base_address(&self) -> u64 {
-        self.api.peb_base_address(&self.pid)
+        self.api.peb_base_address(&self.context)
     }
 
     fn pid(&self) -> u32 {
-        self.api.pid(&self.pid)
+        self.api.pid(&self.context)
     }
 }
