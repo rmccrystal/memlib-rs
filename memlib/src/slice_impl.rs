be181d8a75660abe0e8bbaa944c8929324892dcc
Ryan McCrystal <rrmccrystal@gmail.com>
2022-06-07 18:06:14 -0700

    Replaced RefCell with Cell for slice_impl.rs

--- memlib/src/slice_impl.rs
+++ memlib/src/slice_impl.rs
@@ -1,4 +1,4 @@
-use std::cell::RefCell;
+use std::cell::{Cell, RefCell};
 use std::ops::{Deref, DerefMut};
 use super::*;
 
@@ -38,26 +38,64 @@ impl MemoryWrite for RefCell<[u8]> {
     }
 }
 
-impl<'a, T: Deref<Target=&'a [u8]>> MemoryRead for RefCell<T> {
+impl<T: AsRef<[u8]> + Copy> MemoryRead for Cell<T> {
     fn try_read_bytes_into(&self, address: u64, buffer: &mut [u8]) -> Option<()> {
-        if address as usize + buffer.len() > self.borrow().len() {
+        if address as usize + buffer.len() > self.get().as_ref().len() {
             return None;
         }
 
-        buffer.copy_from_slice(&self.borrow()[address as usize..address as usize + buffer.len()]);
+        buffer.copy_from_slice(&self.get().as_ref()[address as usize..address as usize + buffer.len()]);
 
         Some(())
     }
 }
 
-impl<'a, T: DerefMut<Target=&'a mut [u8]>> MemoryWrite for RefCell<T> {
+impl<T: AsMut<[u8]> + Copy> MemoryWrite for Cell<T> {
     fn try_write_bytes(&self, address: u64, buffer: &[u8]) -> Option<()> {
-        if address as usize + buffer.len() > self.borrow().len() {
+        if address as usize + buffer.len() > self.get().as_mut().len() {
             return None;
         }
 
-        self.borrow_mut()[address as usize..address as usize + buffer.len()].copy_from_slice(buffer);
+        self.get().as_mut()[address as usize..address as usize + buffer.len()].copy_from_slice(buffer);
 
         Some(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::*;
+
+    fn use_read(read: impl MemoryRead) {}
+    fn use_write(write: impl MemoryWrite) {}
+    fn use_readwrite(readwrite: impl MemoryRead + MemoryWrite) {}
+
+    #[test]
+    fn test_slice_read() {
+        let mut buffer = [0u8; 10];
+        let slice = &buffer[..];
+        use_read(slice);
+    }
+
+    #[test]
+    fn test_refcell_read() {
+        let mut buffer = [0u8; 10];
+        let cell = Cell::new(buffer);
+        use_read(cell);
+    }
+
+    #[test]
+    fn test_refcell_write() {
+        let mut buffer = [0u8; 10];
+        let cell = Cell::new(buffer);
+        use_write(cell);
+    }
+
+    #[test]
+    fn test_refcell_readwrite() {
+        let mut buffer = [0u8; 10];
+        let cell = Cell::new(buffer);
+        use_readwrite(cell);
     }
 }
