b3073521577576b3b747a211eef16e08d33c4a92
Ryan McCrystal <rrmccrystal@gmail.com>
2022-09-22 18:27:53 -0700

    refactor: Remove generic_associated_types feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #![feature(decl_macro)]
 #![feature(associated_type_defaults)]
 #![feature(associated_type_bounds)]
-#![feature(generic_associated_types)]
 
 use alloc::string::{FromUtf16Error, FromUtf8Error};
 use core::mem::MaybeUninit;
